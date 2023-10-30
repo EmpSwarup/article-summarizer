d8da5f82790c3efdba6386e866b2e67854a1b551
Swarup Sapkota <empswarupcoc@gmail.com>
2023-10-30 10:38:28 +0545

    analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 
 import App from "./App";
 import { store } from "./services/store";
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <Analytics />
     </Provider>
   </React.StrictMode>
 );
